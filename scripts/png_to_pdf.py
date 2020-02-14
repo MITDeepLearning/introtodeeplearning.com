581ba3d1ed5140311c04d3226461c6ba5b77c32e
Alexander Amini <xan.amini@gmail.com>
2020-02-13 19:09:50 -0500

    png to transparent to pdf draft but too large, so commenting

--- scripts/png_to_pdf.py
+++ scripts/png_to_pdf.py
@@ -3,11 +3,33 @@ import argparse
 import glob
 import os
 from tqdm import tqdm
+import cv2
+import numpy as np
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help="input directory with pngs")
 parser.add_argument("-o", "--output", help="output pdf file")
 args = parser.parse_args()
+
+
+# def get_most_common_color(img):
+#     return np.array([255, 255, 255])
+#
+# def remove_color(image, color):
+#     channel_mask = [((image[:,:,i]==color[i])) for i in range(3)]
+#     mask = 255*(1-np.prod(channel_mask, axis=0)[:,:,np.newaxis])
+#
+#     rgba = np.concatenate((image, mask), axis=2)
+#     return rgba
+#
+#
+# for image in tqdm(glob.glob(os.path.join(args.input, "*.png"))):
+#     data = cv2.imread(image)
+#     background_color = get_most_common_color(data)
+#     rgba = remove_color(data, background_color)
+#     cv2.imwrite(image+".alpha.png", rgba)
 
 
 pdf = fpdf.FPDF(format=(1280,720))
@@ -19,6 +41,5 @@ for image in tqdm(glob.glob(os.path.join(args.input, "*.png"))):
     pdf.add_page()
     pdf.image(image, x=0, y=0, w=1280, h=720)
     pdf.image("watermark.png", x=0, y=0, w=1280, h=720)
-    
-pdf.output(args.output, "F")
 
+pdf.output(args.output, "F")
